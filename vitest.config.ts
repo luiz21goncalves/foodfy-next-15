26cc6ea854826c7483564b15cffc8587067faab0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-02-16 10:57:31 -0300

    chore: add testing library setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [react(), tsConfigPaths()],
   test: {
     globals: false,
-    reporters: ['verbose']
+    reporters: ['verbose'],
+    environment: 'happy-dom',
+    setupFiles: ['./vitest.setup.ts']
   }
 })
